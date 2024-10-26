d0e10ca7098d992cc1a8580002f949a3dbda2ccb
Josue Zavala <josue.zavala.projects@gmail.com>
2024-10-26 01:06:23 -0600

    Create project foldering

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { HomeScreen } from "./src/presentation/screens/HomeScreens";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello this is my app Reloaded</Text>
+      <HomeScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/presentation/screens/SettingsScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const StackScreen = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 30,
+          textAlign: "center",
+          marginTop: "20%",
+        }}
+      >
+        Stack screen
+      </Text>
+    </View>
+  );
+};
